f45ce895378f371adab2f719efe671115e2bf401
Tate Rathje <taterathje@gmail.com>
2017-05-10 02:54:14 -0700

    Added /sport/:name endpoint & node debugging in package.json

--- server/app.js
+++ server/app.js
@@ -9,26 +9,31 @@ mongoUtil.connect();
 app.use(express.static(__dirname + "/../client"));
 
 app.get("/sports", (req, res) => {
-	let sports = mongoUtil.sports();
-	sports.find().toArray((err,docs) => {
-	 console.log(JSON.stringify(docs));
-	 let sportNames = docs.map((sport) => sport.name);
-	 res.json( sportNames );
-	});
-})
+  let sports = mongoUtil.sports();
+  sports.find().toArray((err, docs) => {
+    console.log(JSON.stringify(docs));
+    let sportNames = docs.map((sport) => sport.name);
+    res.json(sportNames);
+  });
+});
+
+app.get('/sports/:name', (req, res) => {
+  let sportName = req.params.name;
+  console.log('Sport name: ', sportName);
+
+  let sport = {
+    "name": "Cycling",
+    "goldMedals": [{
+      "division": "Men's Sprint",
+      "country": "UK",
+      "year": 2012
+    }, {
+      "division": "Women's Sprint",
+      "country": "Australia",
+      "year": 2012
+    }]
+  };
+  res.json(sport);
+});
 
 app.listen(8181, () => console.log("Listening on 8181"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
